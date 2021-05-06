060706a225ed257dc00ff7d19bc9108d2b67ab9b
Muhamad Syafii <muhamadsyafii@cx.network>
2021-05-06 13:44:15 +0700

    add class decimal number

---
+++ src/DecimalNumber.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by Muhamad Syafii
+ * Thursday, 6/5/2021
+ * Copyright (c) 2021.
+ * All Rights Reserved
+ */
+
+fun main() {
+    val x = 7
+    val y = 8f
+    val result = (x/y)
+
+    println("The value of $x divided by $y is ${x/y } ")
+
+    println()
+    println("$result")
+    println()
+    println("Done")
+}
