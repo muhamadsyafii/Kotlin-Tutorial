d3ce19b7e5faaf8c060c56834293d1e2c9729442
Muhamad Syafii <muhamadsyafii@cx.network>
2021-05-06 13:24:48 +0700

    add class variable type

---
+++ src/VariableType.kt
@@ -0,0 +1,21 @@
+/*
+ * Created by Muhamad Syafii
+ * Thursday, 6/5/2021
+ * Copyright (c) 2021.
+ * All Rights Reserved
+ */
+
+fun main() {
+    //if use var u can change value from variable
+    var varVariable = 5
+    val valVariable = 6
+    var varBoolean = false
+    println("Var : The value of our variable is : $varVariable")
+    println("Val : The value of our variable is : $valVariable")
+    println("Var Boolean : The value of our variable is : $varBoolean")
+    varVariable = 10
+    varBoolean = true
+    println("Var : The value is now : $varVariable")
+    println("Var Boolean : The value is now : $varBoolean")
+}
+
