6170592473ebe2f77661a31cc3dbb3ee4d64678f
Muhamad Syafii <muhamadsyafii@cx.network>
2021-05-06 13:38:20 +0700

    add clas arithmetic

---
+++ src/Arithmetic.kt
@@ -0,0 +1,18 @@
+/*
+ * Created by Muhamad Syafii
+ * Thursday, 6/5/2021
+ * Copyright (c) 2021.
+ * All Rights Reserved
+ */
+
+fun main() {
+    val a = 6
+    val b = 2
+    val result = a + b
+
+    println("Result : $result")
+    println("Result : " + (a - b))
+    println("Result : ${a * b}")
+    print("Result : " + (2 / 6))
+}
+
