5fe4432d8646adc035421989db07f9cd9d9b5e84
Muhamad Syafii <muhamadsyafii@cx.network>
2021-05-06 16:07:52 +0700

    add class logical and comparasion operator

---
+++ src/LogicalAndComparisonOperator.kt
@@ -0,0 +1,17 @@
+/*
+ * Created by Muhamad Syafii
+ * Thursday, 6/5/2021
+ * Copyright (c) 2021.
+ * All Rights Reserved
+ */
+
+fun main() {
+    val x = 5
+    val y = 5
+
+    val a = 6
+    val b = 10
+
+    println(!(x == y || a == b))
+}
+
