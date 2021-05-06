14970f80e67fdf60164a8c56305b8a4ecc3dfeb3
Muhamad Syafii <muhamadsyafii@cx.network>
2021-05-06 13:51:37 +0700

    add class string

---
+++ src/StringClass.kt
@@ -0,0 +1,19 @@
+/*
+ * Created by Muhamad Syafii
+ * Thursday, 6/5/2021
+ * Copyright (c) 2021.
+ * All Rights Reserved
+ */
+
+fun main() {
+    val text = "this is an example text"
+
+    println("Default : ")
+    println("Our string is : $text")
+    println("UpperCase : ")
+    println("Our string is : ${text.toUpperCase()}")
+    println("LowerCase : ")
+    println("Our string is : ${text.toLowerCase()}")
+
+}
+
